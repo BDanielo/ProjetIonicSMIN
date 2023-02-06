613e543a91872df349786cca0eb153b8a461c8d9
BDanielo <daniel@armieux.fr>
2023-02-06 14:10:02 +0100

    update

--- src/app/services/mtag-api.service.ts
+++ src/app/services/mtag-api.service.ts
@@ -33,14 +33,44 @@ export interface StationsOfLine {
 export class MTAGAPIService {
   // declare url of mtag api
   private mtagApiUrl = 'https://data.mobilites-m.fr/api/';
-  private data = '';
+
   private URLarretsLignes = 'routers/default/index/routes/:id/clusters';
+
   private URLlignes = 'routers/default/index/routes?reseaux=:transport';
 
+  private URLitineraire = 'routers/default/plan';
+
+  // https://data.mobilites-m.fr/api/routers/default/index/clusters/SEM:GENLETOILE/stoptimes?route=SEM%3AA
+
+  private URLhorairesArret =
+    'routers/default/index/clusters/:station/stoptimes?route=:line';
+
+  // https://data.mobilites-m.fr/api/routers/default/plan?routerId=prod&otp=undefined&mode=TRANSIT&showIntermediateStops=true&numItineraries=3&maxWalkDistance=1000&fromPlace=45.18999132364521,5.715137975226607&toPlace=45.18908931392589,5.698438510275174&arriveBy=false&time=11:56&date=2023-02-03&ui_date=vendredi-03-f%C3%A9vrier-03/02/2023&locale=fr_FR&walkReluctance=10
+
   public TramLines: TramLine[] = [];
-  private TramStations: StationsOfLine[] = [];
+  public TramStations: StationsOfLine[] = [];
 
   constructor(private http: HttpClient) {}
+
+  getStopTimesFromStation(station: string, line: string) {
+    let url =
+      this.mtagApiUrl +
+      this.URLhorairesArret.replace(':station', station).replace(':line', line);
+
+    console.log('STOP TIMES | URL : ' + url);
+
+    let data = this.http
+      .get(url, {
+        headers: {
+          Origin: 'https://www.armieux.fr',
+        },
+      })
+      .subscribe((data: any) => {
+        console.log('GET HORAIRE DATA : ');
+        console.log(data);
+        return data;
+      });
+  }
 
   getTramStations(id: string) {
     return this.http.get(
@@ -61,21 +91,69 @@ export class MTAGAPIService {
   }
 
   getAllTramStations() {
-    // for each element of LigneTram, get the stations
-    console.log('test');
-    this.TramStations = [];
-    this.getTramLines().then((data) => {
-      this.TramLines.forEach((element) => {
-        this.getTramStations(element.id).subscribe((data: any) => {
-          let StationsOfLine: StationsOfLine = {
-            Line: element.id,
-            TramStation: data,
-          };
-          this.TramStations.push(StationsOfLine);
+    return new Promise((resolve, reject) => {
+      this.TramStations = [];
+      this.getTramLines().then((data) => {
+        this.TramLines.forEach((element) => {
+          this.getTramStations(element.id).subscribe((data: any) => {
+            let StationsOfLine: StationsOfLine = {
+              Line: element.id,
+              TramStation: data,
+            };
+            this.TramStations.push(StationsOfLine);
+          });
         });
+        console.log('LINES : ');
+        console.log(this.TramLines);
+        console.log('STATIONS : ');
+        console.log(this.TramStations);
+        resolve(true);
       });
-      console.log(this.TramLines);
-      console.log(this.TramStations);
     });
+  }
+
+  calcItinerary(
+    LatSource: number,
+    LonSource: number,
+    LatTarget: number,
+    LonTarget: number
+  ) {
+    // get current date
+    let dateNow = new Date();
+
+    let time = dateNow.getHours() + ':' + dateNow.getMinutes();
+
+    let date =
+      dateNow.getFullYear() + '-' + dateNow.getMonth() + '-' + dateNow.getDay();
+
+    let url =
+      this.mtagApiUrl +
+      this.URLitineraire +
+      '?routerId=prod' +
+      '&otp=undefined' +
+      '&mode=TRANSIT' +
+      '&showIntermediateStops=true' +
+      '&numItineraries=3' +
+      '&fromPlace=' +
+      LatSource +
+      ',' +
+      LonSource +
+      '&toPlace=' +
+      LatTarget +
+      ',' +
+      LonTarget +
+      '&arriveBy=false' +
+      '&time=' +
+      time +
+      '&date=' +
+      date +
+      '&locale=fr_FR';
+    console.log(url);
+    console.log(this.http.get(url));
+  }
+
+  getTramStationsOfLine(id: string) {
+    this.getAllTramStations();
+    return this.TramStations.find((element) => element.Line === id);
   }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,6 +6,7 @@ import 'leaflet-routing-machine';
 import { Geolocation, Position } from '@capacitor/geolocation';
 import {
   MTAGAPIService,
+  StationsOfLine,
   TramLine,
   TramStation,
 } from '../services/mtag-api.service';
@@ -20,288 +21,6 @@ declare var L: any;
 export class Tab1Page {
   map: L.Map | undefined;
 
-  private tramA = [
-    {
-      id: 'SEM:LETOILE',
-      code: 'SEM:GENLETOILE',
-      city: 'Le Pont-de-Claix',
-      name: "L'Etoile",
-      visible: true,
-      lat: 45.13788,
-      lon: 5.7033,
-    },
-    {
-      id: 'SEM:CHANDON',
-      code: 'SEM:GENCHANDON',
-      city: 'Le Pont-de-Claix',
-      name: 'Edmée Chandon',
-      visible: true,
-      lat: 45.13671,
-      lon: 5.7083,
-    },
-    {
-      id: 'SEM:DENPAPIN',
-      code: 'SEM:GENDENPAPIN',
-      city: 'Échirolles',
-      name: 'Denis Papin',
-      visible: true,
-      lat: 45.13832,
-      lon: 5.71307,
-    },
-    {
-      id: 'SEM:ADELAUNE',
-      code: 'SEM:GENADELAUNE',
-      city: 'Échirolles',
-      name: 'Auguste Delaune',
-      visible: true,
-      lat: 45.14292,
-      lon: 5.71527,
-    },
-    {
-      id: 'SEM:MARIECUR',
-      code: 'SEM:GENMARIECUR',
-      city: 'Échirolles',
-      name: 'Marie Curie',
-      visible: true,
-      lat: 45.14523,
-      lon: 5.71704,
-    },
-    {
-      id: 'SEM:RAMPE',
-      code: 'SEM:GENRAMPE',
-      city: 'Échirolles',
-      name: 'La Rampe - Centre-Ville',
-      visible: true,
-      lat: 45.14948,
-      lon: 5.71818,
-    },
-    {
-      id: 'SEM:ECHIGARE',
-      code: 'SEM:GENECHIGARE',
-      city: 'Échirolles',
-      name: 'Echirolles Gare',
-      visible: true,
-      lat: 45.15302,
-      lon: 5.71848,
-    },
-    {
-      id: 'SEM:ESSARTS',
-      code: 'SEM:GENESSARTS',
-      city: 'Échirolles',
-      name: 'Essarts - La Butte',
-      visible: true,
-      lat: 45.15588,
-      lon: 5.71909,
-    },
-    {
-      id: 'SEM:SURIEUX',
-      code: 'SEM:GENSURIEUX',
-      city: 'Échirolles',
-      name: 'Surieux',
-      visible: true,
-      lat: 45.15846,
-      lon: 5.72192,
-    },
-    {
-      id: 'SEM:GRANGES',
-      code: 'SEM:GENGRANGES',
-      city: 'Échirolles',
-      name: 'Les Granges',
-      visible: true,
-      lat: 45.15696,
-      lon: 5.72781,
-    },
-    {
-      id: 'SEM:POLESUD',
-      code: 'SEM:GENPOLESUD',
-      city: 'Grenoble',
-      name: 'Polesud - Alpexpo',
-      visible: true,
-      lat: 45.15715,
-      lon: 5.73384,
-    },
-    {
-      id: 'SEM:GRDPLACE',
-      code: 'SEM:GENGRDPLACE',
-      city: 'Grenoble',
-      name: "Grand'place",
-      visible: true,
-      lat: 45.15893,
-      lon: 5.73179,
-    },
-    {
-      id: 'SEM:ARLEQUIN',
-      code: 'SEM:GENARLEQUIN',
-      city: 'Grenoble',
-      name: 'Arlequin',
-      visible: true,
-      lat: 45.16359,
-      lon: 5.73047,
-    },
-    {
-      id: 'SEM:BRUYERE',
-      code: 'SEM:GENBRUYERE',
-      city: 'Grenoble',
-      name: 'La Bruyère-Parc Jean Verlhac',
-      visible: true,
-      lat: 45.16635,
-      lon: 5.73118,
-    },
-    {
-      id: 'SEM:MALHERBE',
-      code: 'SEM:GENMALHERBE',
-      city: 'Grenoble',
-      name: 'Malherbe',
-      visible: true,
-      lat: 45.16928,
-      lon: 5.73229,
-    },
-    {
-      id: 'SEM:MAICULTU',
-      code: 'SEM:GENMAICULTU',
-      city: 'Grenoble',
-      name: 'MC2: Maison de la Culture',
-      visible: true,
-      lat: 45.17347,
-      lon: 5.73206,
-    },
-    {
-      id: 'SEM:MOUNIER',
-      code: 'SEM:GENMOUNIER',
-      city: 'Grenoble',
-      name: 'Mounier',
-      visible: true,
-      lat: 45.179,
-      lon: 5.73179,
-    },
-    {
-      id: 'SEM:ALB1BELG',
-      code: 'SEM:GENALB1BELG',
-      city: 'Grenoble',
-      name: 'Albert 1er de Belgique',
-      visible: true,
-      lat: 45.18168,
-      lon: 5.73167,
-    },
-    {
-      id: 'SEM:CHAVANT',
-      code: 'SEM:GENCHAVANT',
-      city: 'Grenoble',
-      name: 'Chavant',
-      visible: true,
-      lat: 45.18466,
-      lon: 5.73177,
-    },
-    {
-      id: 'SEM:VERDUN',
-      code: 'SEM:GENVERDUN',
-      city: 'Grenoble',
-      name: 'Verdun - Préfecture',
-      visible: true,
-      lat: 45.1883,
-      lon: 5.73144,
-    },
-    {
-      id: 'SEM:DUBEDOUT',
-      code: 'SEM:GENDUBEDOUT',
-      city: 'Grenoble',
-      name: 'Hubert Dubedout - Maison du Tourisme',
-      visible: true,
-      lat: 45.1902,
-      lon: 5.72822,
-    },
-    {
-      id: 'SEM:VICTHUGO',
-      code: 'SEM:GENVICTHUGO',
-      city: 'Grenoble',
-      name: 'Victor Hugo',
-      visible: true,
-      lat: 45.18938,
-      lon: 5.72499,
-    },
-    {
-      id: 'SEM:ALSACELO',
-      code: 'SEM:GENALSACELO',
-      city: 'Grenoble',
-      name: 'Alsace-Lorraine',
-      visible: true,
-      lat: 45.18911,
-      lon: 5.7193,
-    },
-    {
-      id: 'SEM:GARES',
-      code: 'SEM:GENGARES',
-      city: 'Grenoble',
-      name: 'Gares',
-      visible: true,
-      lat: 45.19078,
-      lon: 5.71554,
-    },
-    {
-      id: 'SEM:STBRUNO',
-      code: 'SEM:GENSTBRUNO',
-      city: 'Grenoble',
-      name: 'Saint-Bruno',
-      visible: true,
-      lat: 45.18833,
-      lon: 5.71351,
-    },
-    {
-      id: 'SEM:BERRIAT',
-      code: 'SEM:GENBERRIAT',
-      city: 'Grenoble',
-      name: 'Berriat-Le Magasin',
-      visible: true,
-      lat: 45.18878,
-      lon: 5.70588,
-    },
-    {
-      id: 'SEM:FONTAINA',
-      code: 'SEM:GENFONTAINA',
-      city: 'Fontaine',
-      name: 'Les Fontainades - Le Vog',
-      visible: true,
-      lat: 45.18909,
-      lon: 5.69844,
-    },
-    {
-      id: 'SEM:MAISONNA',
-      code: 'SEM:GENMAISONNA',
-      city: 'Fontaine',
-      name: 'Louis Maisonnat',
-      visible: true,
-      lat: 45.18912,
-      lon: 5.69409,
-    },
-    {
-      id: 'SEM:FONTAHDV',
-      code: 'SEM:GENFONTAHDV',
-      city: 'Fontaine',
-      name: 'Fontaine Hôtel de Ville - La Source',
-      visible: true,
-      lat: 45.19134,
-      lon: 5.68727,
-    },
-    {
-      id: 'SEM:CHARLMIC',
-      code: 'SEM:GENCHARLMIC',
-      city: 'Fontaine',
-      name: 'Charles Michels',
-      visible: true,
-      lat: 45.19385,
-      lon: 5.68009,
-    },
-    {
-      id: 'SEM:LAPOYA',
-      code: 'SEM:GENLAPOYA',
-      city: 'Fontaine',
-      name: 'La Poya',
-      visible: true,
-      lat: 45.19709,
-      lon: 5.67222,
-    },
-  ];
-
   lignesTram: TramLine[] = [];
 
   constructor(public MtagService: MTAGAPIService) {}
@@ -313,7 +32,7 @@ export class Tab1Page {
 
     // create map
     this.map = leaflet
-      .map('map')
+      .map('map', { zoomControl: false })
       .setView([45.19270700749426, 5.718059703818313], 20);
     leaflet
       .tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -332,13 +51,13 @@ export class Tab1Page {
     this.getLocation();
 
     // add marker for each tramA element
-    this.tramA.forEach((element) => {
-      const marker = leaflet
-        .marker([element.lat, element.lon])
-        .addTo(this.map!);
-      marker.bindPopup(element.name);
-      this.map!.addLayer(marker);
-    });
+    // this.tramA.forEach((element) => {
+    //   const marker = leaflet
+    //     .marker([element.lat, element.lon])
+    //     .addTo(this.map!);
+    //   marker.bindPopup(element.name);
+    //   this.map!.addLayer(marker);
+    // });
 
     // L.Routing.control({
     //   waypoints: [
@@ -357,7 +76,20 @@ export class Tab1Page {
     //   console.log(this.lignesTram);
     // });
 
-    this.MtagService.getAllTramStations();
+    this.MtagService.calcItinerary(
+      45.19270700749426,
+      5.718059703818313,
+      45.189162995391825,
+      5.696816464474088
+    );
+
+    this.MtagService.getAllTramStations().then((data: any) => {
+      this.markEveryStation();
+    });
+
+    console.log(
+      this.MtagService.getStopTimesFromStation('SEM:GENLETOILE', 'SEM:A')
+    );
   }
 
   getLocation(): Promise<Position> {
@@ -381,6 +113,23 @@ export class Tab1Page {
         .catch((error) => {
           reject(error);
         });
+    });
+  }
+
+  markStation(station: TramStation) {
+    const marker = leaflet.marker([station.lat, station.lon]).addTo(this.map!);
+    marker.bindPopup(station.name);
+    this.map!.addLayer(marker);
+  }
+
+  markEveryStation() {
+    console.log('MARK EVERY STATION');
+    this.MtagService.TramStations.forEach((Line) => {
+      console.log(Line);
+      Line.TramStation.forEach((station) => {
+        console.log(station);
+        this.markStation(station);
+      });
     });
   }
 }
