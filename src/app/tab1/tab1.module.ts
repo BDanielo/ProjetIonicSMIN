5f1762b75813e0c215fbbf8ddf7e109c64f78688
BDanielo <daniel@armieux.fr>
2023-02-06 14:26:32 +0100

    Merge repaired

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,13 +1,16 @@
 import { IonicModule } from '@ionic/angular';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  NgModule,
+  NO_ERRORS_SCHEMA,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
-import { DropDownScheduleComponent } from '../drop-down-schedule/drop-down-schedule.component';
-
+// import { DropDownScheduleComponent } from '../drop-down-schedule/drop-down-schedule.component';
 
 @NgModule({
   imports: [
@@ -15,15 +18,12 @@ import { DropDownScheduleComponent } from '../drop-down-schedule/drop-down-sched
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
   ],
   declarations: [
-    Tab1Page, 
-    DropDownScheduleComponent
+    Tab1Page,
+    // DropDownScheduleComponent
   ],
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA,
-    NO_ERRORS_SCHEMA
-  ]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
 export class Tab1PageModule {}
--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -1,9 +1,14 @@
 import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  NgModule,
+  NO_ERRORS_SCHEMA,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { DropDownScheduleComponent } from '../drop-down-schedule/drop-down-schedule.component';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 
@@ -13,8 +18,9 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    Tab2PageRoutingModule,
   ],
-  declarations: [Tab2Page]
+  declarations: [Tab2Page, DropDownScheduleComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
 export class Tab2PageModule {}
